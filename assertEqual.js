d5c242628e236384d2ea9d454bfa0a38929cc06a
Noah <lauffern@gmail.com>
2022-02-07 22:43:23 +0000

    assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log('✅✅✅Assertion passed: ' + actual + " === " + expected);
+  else if (actual !== expected)
+    console.log('🛑🛑🛑 Assertion failed: ' + actual + " !== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(69, 69);
