511db06f82f213d2349f13d7158c02ae27fd9767
Noah <lauffern@gmail.com>
2022-02-09 00:27:25 +0000

    lint fix

--- without.js
+++ without.js
@@ -15,22 +15,22 @@ const eqArrays = function(arr1, arr2) {
 };
 
 const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true){
+  if (eqArrays(arr1, arr2) === true) {
     console.log(`✅✅✅Assertion passed: ${arr1} === ${arr2}`);
-  }else if (eqArrays(arr1, arr2) === false){
-  console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`);
+  } else if (eqArrays(arr1, arr2) === false) {
+    console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`);
   }
 };
 
 const without = function(source, itemsToRemove) {
-  const outputArr = [] 
+  const outputArr = [];
   for (let i = 0; i < source.length; i++) {
-    if (!itemsToRemove.includes(source[i])){ // checks to see if the iterator at [i] includes itemToRemove 
-        outputArr.push(source[i]) //push to new array
-      } 
+    if (!itemsToRemove.includes(source[i])) { // checks to see if the iterator at [i] includes itemToRemove
+      outputArr.push(source[i]); //push to new array
     }
-    return outputArr
+  }
+  return outputArr;
 };
 
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3]) // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3", 3])) // => ["1", "2"]
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3", 3])); // => ["1", "2"]
