b3b33ef90516989ed550ae54e082945371f27231
Noah <lauffern@gmail.com>
2022-02-14 14:51:14 +0000

    Test push

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,6 @@
+const assertArraysEqual = require('../assertArraysEqual')
+
+assertArraysEqual(['hello'],['hello'])
+assertArraysEqual(['1'],['1'])
+assertArraysEqual(['hello',2],['hello'])
+assertArraysEqual(['1','2','3'],['1','2','3'])
