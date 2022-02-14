cb5f9b7e0323ee8717df09f78d1af60d8e9bb66d
Noah <lauffern@gmail.com>
2022-02-14 15:10:12 +0000

    Middle test push

--- middle.js
+++ middle.js
@@ -1,25 +1,4 @@
 
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const middle = function(array) {
   let odd = [];
   let even = [];
@@ -37,10 +16,5 @@ const middle = function(array) {
   return odd;
 };
 
-console.log(middle([1, 2, 3, 4, 5, 6, 7])); // => [4]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2])); // => []
-console.log(middle([1])); // => []
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+module.exports = middle
